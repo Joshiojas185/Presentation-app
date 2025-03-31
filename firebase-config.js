bbb6ce173be4dfcca94a9b9f901249dc91068d2d
Ojas Joshi <146631578+Joshiojas185@users.noreply.github.com>
2025-03-31 17:32:29 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD7nzziYoW0NNC2KbP1z4Qyuelz2rPfE3A",
+    authDomain: "auth-caf27.firebaseapp.com",
+    projectId: "auth-caf27",
+    storageBucket: "auth-caf27.firebasestorage.app",
+    messagingSenderId: "419446307195",
+    appId: "1:419446307195:web:95404a4f931bcbd0f025de"
+};
